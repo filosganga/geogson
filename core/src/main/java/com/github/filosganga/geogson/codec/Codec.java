b053bbc6da0d60c46fc0f38a75b664fcc95be2c4
Filippo De Luca <me@filippodeluca.com>
2013-10-20 14:36:37 +0100

    #2 Adds codec to core

--- core/src/main/java/com/github/filosganga/geogson/codec/Codec.java
+++ core/src/main/java/com/github/filosganga/geogson/codec/Codec.java
@@ -1,4 +1,4 @@
-package com.github.filosganga.geogson.jts;
+package com.github.filosganga.geogson.codec;
 
 import com.github.filosganga.geogson.model.Geometry;
 import com.google.common.base.Optional;
@@ -6,7 +6,7 @@ import com.google.common.base.Optional;
 /**
  * @author Filippo De Luca - me@filippodeluca.com
  */
-public interface JtsCodec<S extends Object, T extends Geometry<?>> {
+public interface Codec<S extends Object, T extends Geometry<?>> {
 
     T toGeometry(S src);
 
