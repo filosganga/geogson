7457fcd04bab0ace76c2c244aaf81d06fa0585ae
Filippo De Luca <me@filippodeluca.com>
2014-11-05 20:49:24 +0000

    #17 integrate github release

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,9 @@
 set -ev
 
 MYDIR="$(dirname "$(readlink -f "$0")")"
+PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
+
+export PROJECT_VERSION
 
 if [ "${TRAVIS_PULL_REQUEST}" != 'false' ]; then
     IS_NOT_PULL_REQUEST=false
@@ -21,11 +24,13 @@ if [ ${IS_MASTER} == true ] && [ ${IS_NOT_PULL_REQUEST} == true ]; then
 
     BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}
 
-    CURRENT_VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
-    NEW_VERSION=$(echo ${CURRENT_VERSION} | sed -e "s/-SNAPSHOT/\.$BUILD_NUMBER/")
 
-    mvn versions:set -DnewVersion=${NEW_VERSION}
+    PROJECT_VERSION=$(echo ${PROJECT_VERSION} | sed -e "s/-SNAPSHOT/\.$BUILD_NUMBER/")
+
+    mvn versions:set -DnewVersion=${PROJECT_VERSION}
     mvn -N versions:update-child-modules
+
+    export PROJECT_VERSION
 fi
 
 mvn clean package
