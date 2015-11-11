78982d39db6639a5d66eabe64c6550a8282735cf
Filippo De Luca <filippo.deluca@quantemplate.com>
2015-11-11 17:26:50 +0000

    #35 something

---
+++ core/src/main/java/com/github/filosganga/geogson/model/Feature.java
@@ -0,0 +1,13 @@
+package com.github.filosganga.geogson.model;
+
+/**
+ *
+ */
+public class Feature {
+
+    private final Geometry<?> geometry;
+
+    private final ImmutableMap<String, ?> attributes;
+
+
+}
