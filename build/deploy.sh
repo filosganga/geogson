e006be91c94bc0b67cdb66455f7cac99df435dab
Filippo De Luca <filippo.deluca@ovoenergy.com>
2017-08-19 09:44:04 +0100

    Add circleci v2 config

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,30 +4,15 @@ set -ev
 
 MYDIR="$(dirname "$(readlink -f "$0")")"
 
-if [ "${TRAVIS_PULL_REQUEST}" != 'false' ]; then
-    IS_NOT_PULL_REQUEST=false
-    PULL_REQUEST=${TRAVIS_PULL_REQUEST}
-else
-    IS_NOT_PULL_REQUEST=true
-fi
+BUILD_NUMBER=${CIRCLE_BUILD_NUM}
 
-if [ "${TRAVIS_BRANCH}" == 'master' ]; then
-    IS_MASTER=true
-else
-    IS_MASTER=false
-fi
+CURRENT_VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
+NEW_VERSION=$(echo ${CURRENT_VERSION} | sed -e "s/-SNAPSHOT/\.$BUILD_NUMBER/")
 
-if [ $IS_MASTER == true ] && [ $IS_NOT_PULL_REQUEST == true ]; then
+mvn versions:set -DnewVersion=${NEW_VERSION}
+mvn -N versions:update-child-modules
+mvn -s ${MYDIR}/settings.xml -DskipUnitTests=true deploy
 
-    BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}
-
-    CURRENT_VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
-    NEW_VERSION=$(echo ${CURRENT_VERSION} | sed -e "s/-SNAPSHOT/\.$BUILD_NUMBER/")
-
-    mvn versions:set -DnewVersion=${NEW_VERSION}
-    mvn -N versions:update-child-modules
-    mvn -s ${MYDIR}/settings.xml -DskipUnitTests=true deploy
-fi
 
 
 
